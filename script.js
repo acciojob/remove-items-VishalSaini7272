b59a806aac59ec43cd2c00b7e7eefdbcba6e912f
VishalSaini7272 <sainivishal156@gmail.com>
2025-09-14 22:04:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let button = document.querySelector("#removeBtn");
+let select = document.querySelector("#colorSelect");
+
+button.addEventListener("click",()=>{
+	if(select.selectedUndex !== -1){
+		select.remove(select.selectedIndex);
+	}
+})
